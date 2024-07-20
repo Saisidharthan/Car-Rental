33df0793d04ad429daf24651cb67931133bd6628
Sai Sidharthan <saisidharthan@gamil.com>
2024-07-20 21:04:14 +0530

    the clerk authentication initiated-3

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const clerkConfig = {
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:'pk_test_cGVyZmVjdC1yYWNlci03MC5jbGVyay5hY2NvdW50cy5kZXYk',
-CLERK_SECRET_KEY:'sk_test_ewFSRuSG3iba9ZHtbCtazsDEKAaScLRn4YXwMKqK1T'
-};
-
-export default clerkMiddleware(clerkConfig);
+export default clerkMiddleware();
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
