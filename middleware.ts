02e70fa35584ad5e3cb682198ca45278bea101cd
Sai Sidharthan <saisidharthan@gamil.com>
2024-07-20 20:45:33 +0530

    the clerk authentication initiated-2

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Add your Clerk configuration
 const clerkConfig = {
-  publishableKey: 'pk_test_cGVyZmVjdC1yYWNlci03MC5jbGVyay5hY2NvdW50cy5kZXYk',
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:'pk_test_cGVyZmVjdC1yYWNlci03MC5jbGVyay5hY2NvdW50cy5kZXYk',
+CLERK_SECRET_KEY:'sk_test_ewFSRuSG3iba9ZHtbCtazsDEKAaScLRn4YXwMKqK1T'
 };
 
-// Create a custom middleware function that includes the Clerk configuration
 export default clerkMiddleware(clerkConfig);
 
 export const config = {
